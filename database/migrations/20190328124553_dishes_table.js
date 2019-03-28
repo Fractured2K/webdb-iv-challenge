0a7fb09b777a8ab4383b27b0293d916716d4ed2c
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-28 12:41:26 -0700

    Create dishes table

---
+++ database/migrations/20190328124553_dishes_table.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+	return knex.schema.createTable("dishes", tbl => {
+		tbl.increments();
+		tbl.string("dish_name", 255)
+			.notNullable()
+			.unique();
+	});
+};
+
+exports.down = function(knex) {
+	knex.schema.dropTableIfExists("dishes");
+};
